d9ea527a51a8ed5d60126c7278a7accf973c335e
jlu <lujay111@hotmail.com>
2020-01-01 20:54:20 -0800

    1st test

---
+++ test/tcc-sim.js
@@ -0,0 +1,40 @@
+var request = require('request');
+var inspect = require('util').inspect;
+var tcsUtil = require('./tcsUtils');
+
+
+var dstHost = process.argv[2];
+var dstPort = process.argv[3];
+
+var destURL = 'http://'+  dstHost + ':' + dstPort;
+tcsUtil.setOutputLevel("debug"); 
+
+
+var buf = Buffer.alloc(1024, '');
+var dstOptions = {
+	url: destURL,
+	method: 'POST',
+	headers: {'Content-Type': 'text/xml'},
+	body:    buf
+	};
+
+tcsUtil.SHOW_AppOutput("debug",'\nNode.js TCC simulator.\nUsage: node tcc-sim.js <dest host> <dest port>\n');
+tcsUtil.SHOW_AppOutput("debug",'\nsending request to: '+ dstOptions.url);
+tcsUtil.SHOW_AppOutput("debug",'sending body: \n' + dstOptions.body);
+
+request(dstOptions, function(err, res, body) {
+    if (err) { throw err; }
+    tcsUtil.SHOW_AppOutput("debug",inspect({
+      err: err,
+      res: {
+        statusCode: res.statusCode
+      },
+	  body: body
+    }));
+	
+	if (res.statusCode === 200)	{
+		tcsUtil.SHOW_AppOutput("debug", "msg sending OK!\n");
+	}	else	{
+		tcsUtil.SHOW_AppOutput("debug", "msg sending failed!\n");
+	}
+});
